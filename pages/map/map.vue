c06f750a5d439df95d450987fdaad60bc91ea8d1
qhs <773165881@qq.com>
2019-12-13 11:06:51 +0800

    当前位置距离服务区过远时，切换到服务区中心。所有版本更新

--- pages/map/map.vue
+++ pages/map/map.vue
@@ -186,6 +186,32 @@
 					this.setLatitude(res.latitude)
 					this.tempjindu = res.longitude
 					this.tempweidu = res.latitude
+					setTimeout(()=> {
+						var citycenter=this.userinfo.cities[0].coordinate
+						var distances=this.getFlatternDistance(citycenter[0],citycenter[1],res.longitude,res.latitude)
+						// var distances=this.getFlatternDistance(119.283383,26.131703,119.285615,26.123959)
+						if(distances/1000>30){
+							uni.showModal({
+								title: '当前距离服务区过远',
+								content: '是否切换到服务区？',
+								showCancel: true,
+								cancelText: '不切换',
+								confirmText: '切换',
+								success: res => {
+									  if (res.confirm) {
+									            this.setLongitude(citycenter[0])
+									            this.setLatitude(citycenter[1])
+									        } else if (res.cancel) {
+									        }
+								},
+								fail: () => {},
+								complete: () => {}
+							});
+							
+						}
+						console.log('距离城市中心点距离为',distances)
+					}, 1000);
+					
 				},
 				fail: (res) => {
 					console.log('fail', res)
@@ -345,6 +371,39 @@
 			...mapMutations(['setSn', 'setBikeid', 'setBikeinfo', 'setLongitude', 'setLatitude', 'setOrderfirstid',
 				'setOrderinfo', 'setMapcovers', 'setInginfo'
 			]),
+			getRad(d){
+				var PI = Math.PI; 
+				return d*PI/180.0;
+			},
+			// 计算当前位置跟城市中心点距离
+			getFlatternDistance(lat1,lng1,lat2,lng2){
+			        var f = this.getRad((lat1 + lat2)/2);
+			        var g = this.getRad((lat1 - lat2)/2);
+			        var l = this.getRad((lng1 - lng2)/2);
+			        
+			        var sg = Math.sin(g);
+			        var sl = Math.sin(l);
+			        var sf = Math.sin(f);
+			        
+			        var s,c,w,r,d,h1,h2;
+			        var a = 6378137;
+			        var fl = 1/298.257;
+			        
+			        sg = sg*sg;
+			        sl = sl*sl;
+			        sf = sf*sf;
+			        
+			        s = sg*(1-sl) + (1-sf)*sl;
+			        c = (1-sg)*(1-sl) + sf*sl;
+			        
+			        w = Math.atan(Math.sqrt(s/c));
+			        r = Math.sqrt(s*c)/w;
+			        d = 2*w*a;
+			        h1 = (3*r -1)/2/c;
+			        h2 = (3*r +1)/2/s;
+			        
+			        return d*(1 + fl*(h1*sf*(1-sg) - h2*(1-sf)*sg));
+			    },
 			manualsgo() {
 				this.getcarinfo()
 			},
