6412e6e31ca5fed42e277a30f55abeac04d87a04
DickLicha <773165881@qq.com>
2021-07-01 10:53:02 +0800

    完整更新矩形车站跟影子车站的创建
    # Conflicts:
    #	pageA/map/map.vue
    #	pageB/repairlist/repairlist.vue

--- pageB/repairlist/repairlist.vue
+++ pageB/repairlist/repairlist.vue
@@ -218,17 +218,23 @@
 								templist.push(tempobj)
 								break
 							case '9':
-								templist = [{
-										name: '创建停车区',
-										val: '',
-										// url: '/pages/map/map',
-										url: `/pageA/map/map?name=创建停车区&type=9.1`
-									},
+								templist = [
+									// {
+									// 	name: '创建停车区',
+									// 	val: '',
+									// 	// url: '/pages/map/map',
+									// 	url: `/pages/map/map?name=创建停车区&type=9.1`
+									// },
 									{
 										name: '创建车站',
 										val: '',
 										url: `/pageA/map/map?name=创建车站&type=9`
 									},
+									{
+											name: '创建停车区',
+											val: '',
+											url: `/pages/map/map?name=创建停车区&type=9.2`
+										},
 								]
 								break	
 							case '10':
